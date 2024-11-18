bb21808dd907a57deab9185e5df42c4f5bd2a28b
bsmallyt <benjaminsmallyt@gmail.com>
2024-11-18 17:27:38 -0600

    changes

--- spell_check/spell_check.js
+++ spell_check/spell_check.js
@@ -1,33 +1,18 @@
-import express from 'express';
 
-const express = require("express");
-const cors = require("cors");
 
-const corsOptions = {
-  origin: "https://bsmallyt.github.io/spell_check/spell_check.html",
-};
 
-const app = express();
-app.use(cors(corsOptions));
-
-const apiUrl = "https://api.itsbensmall.com";
-
-export function spell_check() {
+function spell_check() {
   //const word = document.getElementById("word").value;
   //document.getElementsByID("sug").classList.toggle("show");
 
-  fetch(apiUrl)
+  fetch("https://api.itsbensmall.com")
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
       return response.json();
     })
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+    .then(data => { console.log(data) })
+    .catch(error => { console.error('Error:', error)});
 
 }
