5a654fa6473dd3a6fbc450dd8cd4a6a05d4c64aa
bsmallyt <benjaminsmallyt@gmail.com>
2024-11-18 21:27:06 -0600

    chdjfnsjldfnd

--- spell_check/spellcheck.js
+++ spell_check/spellcheck.js
@@ -1,15 +1,47 @@
-function spell_check() {
-  //const word = document.getElementById("word").value;
-  //document.getElementsByID("sug").classList.toggle("show");
+const url = "http://localhost:8090"
 
-  fetch("http://localhost:8090")
+function spell_check() {
+  //show the list of word suggestions (if not showing)
+  element = document.getElementById("suggestion").classList
+  if (!element.contains("show")) {
+    element.add("show");
+  }
+
+  //set apiUrl with word as parameter
+  const params = {
+    val: "spellcheck",
+    word: document.getElementById("word").value
+  }
+  const apiUrl = new URL(url)
+  Object.keys(params).forEach(key => apiUrl.searchParams.append(key, params[key]));
+
+  //remove items from the list
+  const list = document.getElementById('suggestion');
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+
+  const newItem = document.createElement('li');
+  newItem.textContent = apiUrl;
+  list.appendChild(newItem);
+
+  fetch(apiUrl)
     .then(response => {
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error('response was not ok');
       }
       return response.json();
     })
-    .then(data => { console.log(data) })
+    .then(data => { 
+      data.forEach(word => {
+        const listItem = document.createElement('li');
+        listItem.textContent = word;
+        list.appendChild(listItem);
+      });
+    })
     .catch(error => { console.error('Error:', error)});
+
+  
+  
 
 }
