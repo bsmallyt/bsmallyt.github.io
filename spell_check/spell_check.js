2cbb78dbb2155c36406452ba0b784ac012d59457
bsmallyt <benjaminsmallyt@gmail.com>
2024-11-18 18:00:53 -0600

    chkb

--- spell_check/spell_check.js
+++ spell_check/spell_check.js
@@ -2,7 +2,7 @@ function spell_check() {
   //const word = document.getElementById("word").value;
   //document.getElementsByID("sug").classList.toggle("show");
 
-  fetch("https://cors-test.codehappy.dev/")
+  fetch("https://imagedelivery.net/JFKtCgwTPfudztWkJA41Xw/443a76c4-1d41-4c39-5bda-412524576e00/public")
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
